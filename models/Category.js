f295f9ad2dc57715bf0f3a47cf02ac4aa6d95675
Shubham <tglevelshubh@gmail.com>
2026-03-24 10:30:57 +0530

    category models

---
+++ models/Category.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+    category:{
+        type:String,
+        required: true
+    }
+},{timestamps: true});
+
+export default mongoose.model('category', categorySchema);
