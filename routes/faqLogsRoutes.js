874ec002c896c312a34960437e28d0ec55e88bb0
TGleveAbhi <tglevelabhi@gmail.com>
2026-03-24 18:31:17 +0530

    authorisation added for faqlogs routes

--- routes/faqLogsRoutes.js
+++ routes/faqLogsRoutes.js
@@ -3,7 +3,7 @@ import { createFaqLog, getUserFaqs } from "../controllers/faqLogsController.js";
 import { authorize } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-router.get("/user-faq",authorize("admin"), getUserFaqs);
+router.get("/user-faq", authorize("admin"), getUserFaqs);
 router.post("/create-faqlog", authorize("user"), createFaqLog);
 
 export default router;
